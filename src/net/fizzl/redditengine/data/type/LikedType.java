7c6902cde3279edd1feb74e75dc716d22e0d05f4
fizzl <fizzl@fizzl.net>
2015-03-07 11:20:31 +0200

    Merge final changes from dev repo

--- src/net/fizzl/redditengine/data/type/LikedType.java
+++ src/net/fizzl/redditengine/data/type/LikedType.java
@@ -13,28 +13,42 @@ import com.google.gson.JsonSerializer;
 
 public class LikedType implements Serializable {
 	private static final long serialVersionUID = 7716692876769642655L;
-	boolean liked_boolean = false;
 	int liked = 0;
+	public int getLiked() {
+		return liked;
+	}
+	public void setLiked(int liked) {
+		this.liked = liked;
+	}
+	
+	
 	public static class TypeAdapter implements JsonSerializer<LikedType>, JsonDeserializer<LikedType> {
 		@Override
 		public JsonElement serialize(LikedType src, Type typeOfSrc, JsonSerializationContext context) {
-			// should serialize number first?
-			if(src.liked > 0) {
-				return new JsonPrimitive(src.liked);
+			if(src.liked < 0) {
+				return new JsonPrimitive(false);
 			}
-			return new JsonPrimitive(src.liked_boolean);
+			if(src.liked > 0) {
+				return new JsonPrimitive(true);
+			}
+			return new JsonPrimitive("null");
 		}
 	
 		@Override
 		public LikedType deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
 			LikedType ret = new LikedType();
 			String raw = json.getAsJsonPrimitive().getAsString();
-			if (!raw.equals("false")) {
-				if (!raw.equals("true")) {
-					ret.liked = Integer.parseInt(raw);  // not false and not true, try number
-				} else {
-					ret.liked_boolean = true;
-				}
+			if(raw.equals("true")) {
+				ret.liked = 1;
+			} 
+			else if(raw.equals("false")) {
+				ret.liked = -1;
+			}
+			else if(raw.equals("null")) {
+				ret.liked = 0;
+			}
+			else {
+				ret.liked = Integer.parseInt(raw);
 			}
 			return ret;
 		}
