9ccbfba48e5152ad68a4ace6361f4eeaf0417939
fizzl <fizzl@fizzl.net>
2015-03-07 11:40:24 +0200

    Add license header to files.

--- src/net/fizzl/redditengine/data/Message.java
+++ src/net/fizzl/redditengine/data/Message.java
@@ -1,4 +1,22 @@
-package net.fizzl.redditengine.data;
+/**
+ * Copyright Maxpower Inc Finland (2014)
+ *
+ * This file is part of RedditEngine.
+ *
+ * RedditEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RedditEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RedditEngine.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+ package net.fizzl.redditengine.data;
 
 /**
  * GSON wrapper for t4 Message
